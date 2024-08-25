362fb376a6bae6560df86316404c432443576665
RH <reganhwang@protonmail.com>
2024-08-25 13:10:01 +0100

    Added rot_test.

--- tests.py
+++ tests.py
@@ -1,27 +1,39 @@
 import random
 from bitwise_funcs import*
-for i in range (20):
-    x = random.randint(0,2**32-1)
-    y = random.randint(0,2**32-1)
-    z = random.randint(0,2**32-1)
-    ch = Ch(x,y,z)
-    xb = format(x, '032b')
-    yb = format(y, '032b')
-    zb = format(z, '032b')
-    chb = format(ch, '032b')
-    
-    
-    for j in range(32):
-        if(xb[j]=='1'):
-            if(chb[j]!=yb[j]):
-                print(f"Error1 at index {j} where x:{x}({xb}), y:{y}({yb}), z:{z}({zb}), ch:{ch}({chb}). chb[j] is {chb[j]} and yb[j] is {yb[j]}")
-                break;
-        else:
-            if(chb[j]!=zb[j]):
-                print(f"Error2 at index {j} where x:{x}({xb}), y:{y}({yb}), z:{z}({zb}), ch:{ch}({chb}). chb[j] is {chb[j]} and zb[j] is {zb[j]}")
+
+def neg_test():
+    for i in range (20):
+        x = random.randint(0,2**32-1)   # For a random integer x,
+        negx = neg(x)
+        assert(x^negx == 2**32-1)       # x ^ neg(x) should equal a 32bit string of 1s.
+
+def rot_test():
+    for i in range (20):
+        x = random.randint(0,2**32-1)
+        n = random.randint(0,31)
+        assert(rotl(n,x)==rotr(32-n,x))
+        
+
+def Ch_test():
+    for i in range (20):
+        # Get random integers and calculate Ch(x,y,z)
+        x = random.randint(0,2**32-1)
+        y = random.randint(0,2**32-1)
+        z = random.randint(0,2**32-1)
+        ch = Ch(x,y,z)
+
+        # Convert to 32bit string
+        xb = format(x, '032b')
+        yb = format(y, '032b')
+        zb = format(z, '032b')
+        chb = format(ch, '032b')
+        
+        # Check condition holds for all 32bits
+        for j in range(32):
+            if(xb[j]=='1'):
+                assert(chb[j]==yb[j])
+            else:
+                assert(chb[j]==zb[j])
+    return None
 
 
-for i in range (20):
-    x = random.randint(0,2**32-1)
-    n = random.randint(0,31)
-    assert(rotl(n,x)==rotr(32-n,x))
