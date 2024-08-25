15d414b3ab0c4bc2e493c3c73417b8f2e0e557e6
RH <reganhwang@protonmail.com>
2024-08-25 02:46:23 +0100

    Added and tested the split_32bit function.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 def get_k(l):
     '''
     Given 'l', 'k' is the smallest non-negative integer satisfying l+1+k = 448 mod 512.
@@ -6,10 +7,47 @@ def get_k(l):
     return (448-n)%512
 
 '''
-import random
+
 for i in range (10):
     l = random.randint(1,10000)
     k = get_k(l)
     assert(k>=0)
     assert((l+1+k)%512 == 448)
+
+'''
+def split_32bit(M):
+    '''
+    Input: string of length 512
+    Output: Array of 16 blocks of 32 bits
+    '''
+    output = []
+    for i in range (16):
+        output.append(M[32*i:32*(i+1)])
+    return output
+
+def make_message_schedule(M):
+    W = split_32bit(M).copy()
+    return 0
+
+'''
+nums = []
+for i in range (32):
+    n = random.randint(0, 2**16-1)
+    nums.append(format(n, '016b'))
+
+msg = ""
+for n in nums:
+    msg = msg+n
+
+M = split_32bit(msg)
+
+for m in M:
+    assert(len(m)==32)
+
+msg2  =""
+
+for m in M:
+    msg2 = msg2+m
+
+assert(msg2==msg)
 '''
