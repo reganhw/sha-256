8d27c2e8cb90363371453b5b8a1bf807523051c7
RH <reganhwang@protonmail.com>
2024-08-25 02:56:08 +0100

    Put preprocessing functions in preprocessing.py.

---
+++ preprocessing.py
@@ -0,0 +1,50 @@
+import random
+from bitwise_funcs import*
+def get_k(l):
+    '''
+    Given 'l', 'k' is the smallest non-negative integer satisfying l+1+k = 448 mod 512.
+    '''
+    n = (l+1)%512
+    return (448-n)%512
+
+'''
+
+for i in range (10):
+    l = random.randint(1,10000)
+    k = get_k(l)
+    assert(k>=0)
+    assert((l+1+k)%512 == 448)
+
+'''
+def split_32bit(M):
+    '''
+    Input: string of length 512
+    Output: Array of 16 blocks of 32 bits
+    '''
+    output = []
+    for i in range (16):
+        block = M[32*i:32*(i+1)]
+        output.append(int(block,2))
+    return output
+
+def make_message_schedule(M):
+    W = split_32bit(M).copy()
+    for t in range(16,80):
+        mid = W[t-3]^W[t-8]^W[t-14]^W[t-16]
+        W.append(rotl(1,mid))
+    return W
+
+'''
+nums = []
+for i in range (16):
+    n = random.randint(0, 2**32-1)
+    nums.append(n)
+
+msg = ""
+for n in nums:
+    msg = msg+format(n, '032b')
+
+M = split_32bit(msg)
+
+assert (nums==M)
+'''
