b7bbb8fea1cd9a6fa81c79181ae1504cdbad62b6
RH <reganhwang@protonmail.com>
2024-08-27 15:48:53 +0100

    Separated update_variables function from sha256.

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ def sha256(M):
     M_padded = padding(M)
     H = [0x6a09e667, 0xbb67ae85,0x3c6ef372,0xa54ff53a, 0x510e527f,0x9b05688c,0x1f83d9ab,0x5be0cd19]
     W = get_message_schedule(M_padded)
-    a,b,c,d,e,f,g,h = H[0], H[1], H[2], H[3], H[4], H[5], H[6], H[7]
+    working_variables = update_variables(W,H)
+    for j in range (8):
+        H[j] = (H[j]+ working_variables[j])%limit
+    return H_to_hex(H)
 
+def update_variables(W,H):
+    a,b,c,d,e,f,g,h = H[0], H[1], H[2], H[3], H[4], H[5], H[6], H[7]
     for t in range (64):
         T1 = (h + Sig1(e) + Ch(e,f,g) + K[t] + W[t])%limit
         T2 = (Sig0(a) + Maj(a,b,c))%limit
@@ -22,19 +27,11 @@ def sha256(M):
         c=b
         b=a
         a = (T1 + T2)%limit
+    return a,b,c,d,e,f,g,h
 
-    
-    values = [a,b,c,d,e,f,g,h]
-    for j in range (8):
-        H[j] = (H[j]+ values[j])%limit
-    
-
-    return H_to_hex(H)
 
 def H_to_hex(H):
     output = ""
     for num in H:
         output = output + format(num, '08x')
     return output
-
-print(sha256("The quick brown fox jumps over the lazy dog"))
