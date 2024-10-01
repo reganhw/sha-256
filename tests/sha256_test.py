fdb690344b500b4eebfaf50c0d65f890c0863895
RH <reganhwang@protonmail.com>
2024-10-01 21:51:42 +0100

    Changed sha256_test to use hashlib.sha256.

--- tests/sha256_test.py
+++ tests/sha256_test.py
@@ -1,25 +1,23 @@
-import random
+import hashlib
+
 from sha256 import *
 from bitwise_funcs import*
+
 def test_single_block():
-    # Reference: https://coding.tools/sha256
-    assert(sha256("")=="e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
-    assert(sha256("a")=="ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb")
-    assert(sha256("!£$%^&")=="eea2da966bc1ee377d4500197d5c694771609f71dd11a7b2fa8c1685daaefd90")
-    assert(sha256("Hello world")=="64ec88ca00b268e5ba1a35678a1b5316d212f4f366b2477232534a8aeca37f3c")
+    text_vectors = ("", "a", "!£$%^&", "Hello world")
+    for v in text_vectors:
+        assert(sha256(v)==hashlib.sha256(v.encode()).hexdigest())
 
 def test_many_blocks():
-    # Reference: https://coding.tools/sha256
 
-    long_str1 = "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"
+    long_str1 = "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq" # 448 bits
     long_str2=("I\'m the golden baby born into the center. Mother was a gun and I fed nature to the beggars. "
     +"Always talking but the kids never listen, they die in the evening, become demons for the weekend.")
     long_str3=("Somebody hooted and they hollered: Can I buy a vowel? "
     +"Don\'t let her catch you in the act of throwing in the towel! "
     +"And when it\'s not as it appears to be the flagrant foul, "
     +"can I put my fingers in your mouth before you start to growl?")
-
-    assert(sha256(long_str1)=="248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1")
-    assert(sha256(long_str2)=="78a7fab979239554f368f42b7c6cffacd52cc9823bd920b1bd40f869713845cc")
-    assert(sha256(long_str3)=="80f48d65f3de367836fecac2fd98c034d3bfcc9d280b374881c0b6bf88e18825")
+    
+    for v in (long_str1, long_str2, long_str3):
+        assert(sha256(v)==hashlib.sha256(v.encode()).hexdigest())
   
