431d62d1a0d3cc54472cccac4005a667cd35dbb6
RH <reganhwang@protonmail.com>
2024-10-01 21:12:06 +0100

    Wrote padding_test.

--- preprocessing_test.py
+++ preprocessing_test.py
@@ -1,5 +1,6 @@
 import random
 from preprocessing import*
+
 def get_k_test():
     for i in range (20):                 # For 20 random integers l,
         l = random.randint(0,10000)
@@ -8,4 +9,13 @@ def get_k_test():
         assert(k<512)                    # k is the smallest non-negative integer satisfying this
         assert(((k+1+l)%512)==448)       # k+1+l :=448 mod 512
 
-get_k_test()
+def padding_test():
+    M = '1'
+    M_padded = padding(M)
+    assert(len(M_padded)%512==0)
+    assert(M_padded[8]=="1")
+    assert(M_padded[-4:-1]+M_padded[-1]=="1000")
+
+if(__name__=='__main__'):
+    get_k_test()
+    padding_test()
